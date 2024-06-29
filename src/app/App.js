d913714f88fa953ae2c6d1ac6fcde7e24038f107
jmadrigal <wku.madrigal@gmail.com>
2024-06-29 02:07:56 -0500

    * Integrated the routing system with the Next.js framework * Created the client and page files for the dynamic routes * Deleted the navbar component * Integrated everything into next.js framework * Fixed Entry point error

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+"use client";
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Navbar from '/navbar';
 import React from 'react';
-import Home from './pages/Home';
-import Shopping from './pages/Shopping';
-import Profile from './pages/Profile';
-import Cart from './pages/Cart';
-import './index.css';
+import Link from 'next/link';
+// import Home from '/pages/home';
+// import Shopping from '/pages/shopping';
+// import Profile from '/pages/profile';
+// import Cart from '/pages/cart';
+// import 'index.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shopping" element={<Shopping />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
-  );
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/shopping" element={<Shopping />} />
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="/cart" element={<Cart />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+export default function App() {
+  return(
+    <div>
+      <Link href="/home">Home</Link>
+      <Link href="/shopping">Shopping</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/cart">Cart</Link>
+    </div>
+  )
 }
-
-export default App;
--- src/app/pages/home/Home.js
+++ src/app/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Intro from "../components/Intro"
-import Signup from "../components/SignUp"
-import Login from "../components/Login"
+import Intro from "../../components/Intro"
+import Signup from "../../components/SignUp"
+import Login from "../../components/Login"
 
 export default function Home() {
 
