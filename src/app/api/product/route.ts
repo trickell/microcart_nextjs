7a2465d15d820e3da0b7dee400bdf3827e6a13d0
jmadrigal <wku.madrigal@gmail.com>
2024-06-29 00:24:17 -0500

    Started master branch for nextjs with microcart
    Co-authored-by: Anna Villarreal <AnnaVi11arrea1@users.noreply.github.com>

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,9 @@
+
+export async function GET() {
+    //var param1 = searchParam.get("id");
+    return new Response('THIS IS SPARTA! ID Number: ');
+}
+
+export async function POST() {
+    return new Response('THIS IS ANNNNAAAAA!');
+}
---
+++ src/app/api/user/route.ts
@@ -0,0 +1,8 @@
+
+export async function GET() {
+    return new Response('THIS IS SPARTA! ID Number: ');
+}
+
+export async function POST() {
+    return new Response('THIS IS ANNNNAAAAA!');
+}
