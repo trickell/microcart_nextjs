417a5bf43f6b141db655a1bcd3e93e24be4328fd
bigxnate <bignaeyt@gmail.com>
2024-06-29 02:46:08 -0700

    Switched to using App Route functionality instead of reac-router-dom

---
+++ src/app/pages/about/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from '../../components/Navbar'
+
+export default function About() {
+    return (
+        <>
+            <h1>About</h1>
+        </>
+    )
+}
--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Intro from "../../components/Intro"
-import Signup from "../../components/SignUp"
+import Signup from "../../components/Signup"
 import Login from "../../components/Login"
 
 export default function Home() {
