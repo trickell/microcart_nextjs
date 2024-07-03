9d1749abbb5a307f461be1db6b22616b990d1a78
jmadrigal <wku.madrigal@gmail.com>
2024-07-02 19:58:50 -0500

    Co-authored-by: Brandon <BearMSU@users.noreply.github.com> Co-authored-by: Anna Villarreal <AnnaVi11arrea1@users.noreply.github.com>

--- src/app/App.js
+++ src/app/App.js
@@ -3,25 +3,6 @@
 // import Navbar from '/navbar';
 import React from 'react';
 import Link from 'next/link';
-// import Home from '/pages/home';
-// import Shopping from '/pages/shopping';
-// import Profile from '/pages/profile';
-// import Cart from '/pages/cart';
-// import 'index.css';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/shopping" element={<Shopping />} />
-//         <Route path="/profile" element={<Profile />} />
-//         <Route path="/cart" element={<Cart />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
 
 export default function App() {
   return(
